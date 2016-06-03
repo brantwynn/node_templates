f6e04af04329babafaf867467ac46e42d3a6c007
Brant Wynn <brant.wynn@acquia.com>
2016-06-03 15:40:33 -0500

    Initial pass including a title field

---
+++ src/Form/ReplicantsBlockForm.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Drupal\replicants\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Entity\EntityFormBuilder;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Url;
+use Drupal\node\Entity\Node;
+use Drupal\node\NodeInterface;
+use Drupal\replicate\Replicator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Class ReplicateNodeBlockForm.
+ *
+ * @package Drupal\replicate_node_block\Form
+ */
+class ReplicantsBlockForm extends FormBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The event dispatcher.
+   *
+   * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
+   */
+  protected $eventDispatcher;
+  
+  /**
+   * The entity form builder.
+   *
+   * @var \Drupal\Core\Entity\EntityFormBuilder
+   */
+  protected $entityFormBuilder;
+  
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, EventDispatcherInterface $event_dispatcher, EntityFormBuilder $entity_form_builder) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->eventDispatcher = $event_dispatcher;
+    $this->entityFormBuilder = $entity_form_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('event_dispatcher'),
+      $container->get('entity.form_builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'replicants_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, NodeInterface $node = NULL) {
+    $form = [];
+    if ($node != NULL && $nid = $node->id()) {
+      $node_form = $this->entityFormBuilder->getForm($node, 'edit');
+      $form['node_id'] = [
+        '#type' => 'hidden',
+        '#value' => $nid
+      ];
+      // @todo: figure out why we cant just set $form['title'] = $node_form['title]
+      $form['title'] = [
+        '#type' => 'textfield',
+        '#default_value' => $node_form['title']['widget'][0]['value']['#default_value'],
+        '#title' => 'Title',
+      ];
+      $form['save'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Save as Copy'),
+      ];
+    }
+    $test = $form;
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Load the original node.
+    $nid = $form_state->getValue('node_id');
+    $node = Node::load($nid);
+    $node_title = $node->getTitle();
+    unset($node);
+    // Create the replicant node.
+    $replicator = new Replicator($this->entityTypeManager, $this->eventDispatcher);
+    if ($replicant = $replicator->replicateByEntityId('node', $nid)) {
+      // Set new values for replicant node.
+      $title = $form_state->getValue('title');
+      $replicant->setTitle($title);
+      $replicant->save();
+      // Send a friendly message.
+      $message = $this->t('"@nodeTitle" [@nodeId] has been copied as "@replicantTitle" [@replicantId].', [
+        '@nodeTitle' => $node_title,
+        '@nodeId' => $nid,
+        '@replicantTitle' => $replicant->getTitle(),
+        '@replicantId' => $replicant->id(),
+      ]);
+      drupal_set_message($message, 'status');
+      // Redirect to the replicant node.
+      $redirectUrl = Url::fromUserInput('/node/' .  $replicant->id());
+      $form_state->setRedirectUrl($redirectUrl);
+    }
+  }
+
+}
---
+++ src/Plugin/Block/ReplicantsBlock.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\replicants\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilder;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a 'ReplicantsBlock' block.
+ *
+ * @Block(
+ *  id = "replicants_block",
+ *  admin_label = @Translation("Replicants"),
+ *  context = {
+ *    "node" = @ContextDefinition("entity:node", label = @Translation("Node"))
+ *  },
+ *  category = @Translation("Forms")
+ * )
+ *
+ */
+class ReplicantsBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The form builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilder
+   */
+  protected $formBuilder;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilder $form_builder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $form_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $block = [];
+    $node = $this->getContextValue('node');
+    $block = [$this->formBuilder->getForm('\Drupal\replicants\Form\ReplicantsBlockForm', $node)];
+    return $block;
+  }
+
+}
