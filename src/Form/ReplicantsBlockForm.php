111f747578862fd8852a46a6c3daf6249696cd46
Brant Wynn <brant.wynn@acquia.com>
2016-06-06 11:03:46 -0500

    Additional check for moderated content.

--- src/Form/ReplicantsBlockForm.php
+++ src/Form/ReplicantsBlockForm.php
@@ -8,6 +8,7 @@ use Drupal\Core\Entity\EntityFormBuilder;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Url;
 use Drupal\node\Entity\Node;
+use Drupal\node\Entity\NodeType;
 use Drupal\node\NodeInterface;
 use Drupal\replicate\Replicator;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -80,15 +81,22 @@ class ReplicantsBlockForm extends FormBase {
       // @todo: figure out why we cant just set $form['title'] = $node_form['title]
       $form['title'] = [
         '#type' => 'textfield',
-        '#default_value' => $node_form['title']['widget'][0]['value']['#default_value'],
+        '#default_value' => $node->getTitle(),
         '#title' => 'Title',
       ];
+      $node_type = NodeType::load($node->getType());
+      if ($node_type->getThirdPartySetting('workbench_moderation', 'enabled')) {
+        $form['draft'] = [
+          '#title' => 'New Draft',
+          '#type' => 'checkbox',
+          '#default_value' => 1
+        ];
+      }
       $form['save'] = [
         '#type' => 'submit',
         '#value' => $this->t('Save as Copy'),
       ];
     }
-    $test = $form;
     return $form;
   }
 
@@ -104,9 +112,17 @@ class ReplicantsBlockForm extends FormBase {
     // Create the replicant node.
     $replicator = new Replicator($this->entityTypeManager, $this->eventDispatcher);
     if ($replicant = $replicator->replicateByEntityId('node', $nid)) {
-      // Set new values for replicant node.
+      // Concatenate url for the replicant node redirect.
+      $url = '/node/' . $replicant->id();
+      // Set title for replicant node.
       $title = $form_state->getValue('title');
       $replicant->setTitle($title);
+      // Set moderation state for replicant node.
+      if ($form_state->getValue('draft')) {
+        // Create a new draft and append to our redirect.
+        $replicant->moderation_state->target_id = 'draft';
+        $url = $url . '/latest';
+      }
       $replicant->save();
       // Send a friendly message.
       $message = $this->t('"@nodeTitle" [@nodeId] has been copied as "@replicantTitle" [@replicantId].', [
@@ -117,7 +133,7 @@ class ReplicantsBlockForm extends FormBase {
       ]);
       drupal_set_message($message, 'status');
       // Redirect to the replicant node.
-      $redirectUrl = Url::fromUserInput('/node/' .  $replicant->id());
+      $redirectUrl = Url::fromUserInput($url);
       $form_state->setRedirectUrl($redirectUrl);
     }
   }
